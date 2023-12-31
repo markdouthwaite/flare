aecbda27fb4039adf21dac937a6d4f2c1befad3a
Mark Douthwaite <mark@douthwaite.io>
2023-12-31 14:32:41 +0000

    Update sources settings to include filters and custom extractors

--- settings.py
+++ settings.py
@@ -12,27 +12,33 @@ SOURCES = {
         "kind": "article",
         "filters": [
             filters.generic.is_english_language,
-            filters.generic.mentions_machine_learning
+            filters.generic.mentions_machine_learning,
         ],
         "url_patterns": {
-            "restrict": [
-                r"https?://(?:\w+\.)?youtube\.com(?:/[\w/#!.-]*)?"
-            ],
+            "restrict": [r"https?://(?:\w+\.)?youtube\.com(?:/[\w/#!.-]*)?"],
             "allow": [
                 # r"https?://(?:\w+\.)?douthwaite\.io(?:/[\w/#!.-]*)?"
             ],
         },
     },
-    "github": {"kind": "code", "extractor": github.extract, "filters": [
-        filters.github.is_popular_repo,
-        filters.github.is_not_malware_repo,
-        filters.github.is_active_repo,
-        # filters.github.is_python_repo,
-        filters.generic.is_english_language,
-        filters.generic.mentions_machine_learning
-    ]},
-    "arxiv": {"kind": "paper", "extractor": arxiv.extract, "filters": [
-        filters.generic.is_english_language,
-        filters.generic.mentions_machine_learning
-    ]},
+    "github": {
+        "kind": "code",
+        "extractor": github.extract,
+        "filters": [
+            filters.github.is_popular_repo,
+            filters.github.is_not_malware_repo,
+            filters.github.is_active_repo,
+            # filters.github.is_python_repo,
+            filters.generic.is_english_language,
+            filters.generic.mentions_machine_learning,
+        ],
+    },
+    "arxiv": {
+        "kind": "paper",
+        "extractor": arxiv.extract,
+        "filters": [
+            filters.generic.is_english_language,
+            filters.generic.mentions_machine_learning,
+        ],
+    },
 }
