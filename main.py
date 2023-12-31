36c224da91528b32a04209137421426a22b13b19
Mark Douthwaite <mark@douthwaite.io>
2023-12-31 14:28:17 +0000

    Remove dead code

--- main.py
+++ main.py
@@ -9,7 +9,5 @@ posts_repo = settings.POST_REPOSITORY
 app = Flask(__name__)
 app.config.update({"sources": sources, "posts_repo": posts_repo})
 app.register_blueprint(posts_blueprint)
-# extract_and_load_post("https://github.com/markdouthwaite/xanthus", sources["github"], repo)
-# print(repo.list())
 
 app.run(port=8080)
