8d9dcf5d860ce3d93f96dfe8764404a619fc915b
Mark Douthwaite <mark@douthwaite.io>
2023-12-31 14:32:02 +0000

    Update error handling for posts command

--- src/commands/posts.py
+++ src/commands/posts.py
@@ -1,13 +1,11 @@
 from src.entities import Post, Source
 from src.interfaces import PostRepository
 from src.common.posts import to_post
+from src.common.extract.errors import UrlExtractError
 from src.common.extract.validate import valid_url
 
 
-def extract_post(url: str, source: Source, validate_target_url: bool = True) -> Post:
-    if validate_target_url and not valid_url(url, source):
-        raise ValueError("invalid url provided")
-
+def extract_post(url: str, source: Source) -> Post:
     extracted_item = source.extractor(url)
     post = to_post(extracted_item, source)
     return post
@@ -33,9 +31,20 @@ def extract_and_load_post(
     filtered: bool = True,
     validate_target_url: bool = True,
 ) -> None:
-    post = extract_post(url, source, validate_target_url=validate_target_url)
+    if validate_target_url and not valid_url(url, source):
+        raise UrlExtractError("invalid url provided")
+
+    post = extract_post(url, source)
 
     if filtered and filter_post(post, source):
-        raise ValueError("cannot load extracted post: the post failed filter checks")
+        raise UrlExtractError(
+            "cannot load extracted post: the post failed filter checks"
+        )
     else:
-        load_post(post, repo)
+        try:
+            load_post(post, repo)
+        except ValueError:
+            raise UrlExtractError(
+                "cannot load extracted post: "
+                "the post could not be loaded into the target repository"
+            )
