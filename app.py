d39cf6ad2f6b73fe49470c2804f06afd35fcdf8f
Mark Douthwaite <mark@douthwaite.io>
2024-01-04 22:31:18 +0000

    Add support for celery job to `/posts/extract` handler.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from src.common.sources import load_sources
 from flask import Flask
+from celery import Celery
 from src.infrastructure.api.posts import posts_blueprint
+from src.infrastructure.api.errors import error_handler_blueprint
 import settings
 
 sources = load_sources(settings)
@@ -8,8 +10,13 @@ posts_repo = settings.POST_REPOSITORY
 
 app = Flask(__name__)
 app.config.update({"sources": sources, "posts_repo": posts_repo})
+app.config["CELERY_BROKER_URL"] = "redis://localhost:6379/0"
+app.config["CELERY_RESULT_BACKEND"] = "redis://localhost:6379/0"
+
+celery = Celery(app.name, broker=app.config["CELERY_BROKER_URL"])
+celery.conf.update(app.config)
+app.celery = celery
 app.register_blueprint(posts_blueprint)
-# extract_and_load_post("https://github.com/markdouthwaite/xanthus", sources["github"], repo)
-# print(repo.list())
+app.register_blueprint(error_handler_blueprint)
 
 app.run(port=8080)
--- src/infrastructure/api/posts.py
+++ src/infrastructure/api/posts.py
@@ -1,9 +1,8 @@
 import json
 from dataclasses import asdict
 from flask import Blueprint, request, current_app, Response
-from src.commands.posts import extract_and_load_post
-from src.common.extract.errors import UrlExtractError
-from src.entities import ErrorMessage, SuccessMessage
+from src.entities import SuccessMessage
+from src.common.identifier import generate_id
 
 
 posts_blueprint = Blueprint("posts", __name__)
@@ -12,25 +11,44 @@ posts_blueprint = Blueprint("posts", __name__)
 @posts_blueprint.post("/posts/extract")
 def extract_and_load_posts():
     payload = request.json
-    try:
-        extract_and_load_post(
-            payload["url"],
-            source=current_app.config["sources"][payload.get("source", "default")],
-            repo=current_app.config["posts_repo"],
-        )
-        message = SuccessMessage(
-            title="success", message="successfully extracted target url"
-        )
-        status = 200
+    post_id = generate_id()
 
-    except UrlExtractError as e:
-        message = ErrorMessage(
-            title="error", message=f"failed to extract target url: {e}"
-        )
-        status = 400
+    current_app.celery.send_task(
+        "src.infrastructure.tasks.extract_and_load_post",
+        args=(post_id, payload["url"]),
+        kwargs={
+            "source": payload.get("source", "default"),
+        },
+    )
+
+    message = SuccessMessage(
+        title="extract-submit-success",
+        message=f"successfully submitted for extraction: {post_id}",
+    )
+    status = 200
 
     return Response(
         status=status,
         response=json.dumps(asdict(message)),
         content_type="application/json",
+    )
+
+
+@posts_blueprint.get("/posts/<post_id>")
+def get_extracted_post(post_id: str):
+    posts_repo = current_app.config["posts_repo"]
+    post = posts_repo.get(post_id)
+    return Response(
+        json.dumps(asdict(post), default=lambda _: str(_)),
+        content_type="application/json"
+    )
+
+
+@posts_blueprint.get("/posts")
+def get_extracted_posts():
+    posts_repo = current_app.config["posts_repo"]
+    posts = posts_repo.list()
+    return Response(
+        json.dumps([asdict(post) for post in posts], default=lambda _: str(_)),
+        content_type="application/json"
     )
