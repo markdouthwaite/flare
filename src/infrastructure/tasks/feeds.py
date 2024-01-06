0f64dda259aba156cf3121f483bede1a0df0065a
Mark Douthwaite <mark@douthwaite.io>
2024-01-06 21:40:44 +0000

    Add background (celery) task for creating or updating a feed

---
+++ src/infrastructure/tasks/feeds.py
@@ -0,0 +1,12 @@
+from celery import shared_task, current_app
+from src.commands.feeds import create_or_update_feed as _create_or_update_feed
+
+
+@shared_task(ignore_result=False)
+def create_or_update_feed(name: str):
+    build_config = current_app.conf.FEEDS[name]
+    posts_repo = current_app.conf.POSTS_REPO
+    feeds_repo = current_app.conf.FEEDS_REPO
+    _create_or_update_feed(
+        name, build_config=build_config, feeds_repo=feeds_repo, posts_repo=posts_repo
+    )
