7c106434c05b4134fca73d27694cc9e31a3ba298
Mark Douthwaite <mark@douthwaite.io>
2024-01-06 20:38:42 +0000

    Add status checking endpoint, explicitly return the task ID rather than Flare-generated post ID.

--- src/infrastructure/api/posts.py
+++ src/infrastructure/api/posts.py
@@ -1,6 +1,8 @@
 import json
 from dataclasses import asdict
 from flask import Blueprint, request, current_app, Response
+from celery import current_app as current_celery_app
+from celery.result import AsyncResult
 from src.entities.errors import SuccessMessage
 from src.common.identifier import generate_id
 from src.infrastructure.tasks.posts import extract_and_load_post
@@ -14,19 +16,51 @@ def extract_and_load_posts():
     payload = request.json
     post_id = generate_id()
 
-    extract_and_load_post.delay(
+    task = extract_and_load_post.delay(
         post_id, payload["url"], source_name=payload.get("source", "default")
     )
 
     message = SuccessMessage(
         title="extract-submit-success",
-        message=f"successfully submitted task '{post_id}' for extraction",
+        message=f"successfully submitted task '{task.id}' for extraction",
     )
 
     return Response(
         response=json.dumps(asdict(message)),
         content_type="application/json",
     )
+
+
+@posts_blueprint.get("/posts/extract/status/<task_id>")
+def get_post_extraction_status(task_id: str):
+    task = AsyncResult(task_id, app=current_celery_app)
+    if task.successful():
+        return Response(
+            json.dumps(
+                {
+                    "title": "successful-extraction",
+                    "message": "successfully extracted target url"
+                }
+            )
+        )
+    elif task.failed():
+        return Response(
+            json.dumps(
+                {
+                    "title": "failed-extraction",
+                    "message": "failed to extract target url"
+                }
+            )
+        )
+    else:
+        return Response(
+            json.dumps(
+                {
+                    "title": "pending-extraction",
+                    "message": "target url is being extracted, please wait."
+                }
+            )
+        )
 
 
 @posts_blueprint.get("/posts/<post_id>")
