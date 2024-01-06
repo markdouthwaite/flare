634b4f75b9a2558d707489b34554264563a46e39
Mark Douthwaite <mark@douthwaite.io>
2024-01-06 21:17:58 +0000

    Refactor extract_and_load_posts function to do more filtering before running more expensive predictions (e.g. calls to GPT-4)

--- src/commands/posts.py
+++ src/commands/posts.py
@@ -1,21 +1,25 @@
-from src.entities import Post, Source
+from src.entities import Post, Source, ExtractedItem
 from src.interfaces import PostRepository
 from src.common.posts import to_post
 from src.common.extract.errors import UrlExtractError
 from src.common.extract.validate import valid_url
 
 
-def extract_post(post_id: str, url: str, source: Source) -> Post:
-    extracted_item = source.extractor(url)
-    post = to_post(post_id, extracted_item, source)
-    return post
+class UrlExtractFilterException(Exception):
+    pass
 
 
-def filter_post(post: Post, source: Source) -> bool:
+def filter_item(
+    item: ExtractedItem, source: Source, raise_exception: bool = True
+) -> bool:
     for f in source.filters:
-        if not f(post):
-            print(f)
-            return True
+        if not f(item):
+            if raise_exception:
+                raise UrlExtractFilterException(
+                    f"target url failed filter '{f.__name__}'"
+                )
+            else:
+                return True
 
     return False
 
@@ -24,28 +28,60 @@ def load_post(post: Post, repo: PostRepository) -> None:
     repo.insert(post)
 
 
+def extract_post(
+    post_id: str,
+    url: str,
+    source: Source,
+    validate_target_url: bool = True,
+    filtered: bool = True,
+):
+    if validate_target_url and not valid_url(url, source):
+        raise UrlExtractError("invalid url provided")
+
+    extracted_item = source.extractor(url)
+
+    if filtered and filter_item(extracted_item, source):
+        raise UrlExtractError(
+            "cannot load extracted post: the post failed filter checks"
+        )
+
+    post = to_post(post_id, extracted_item, source)
+
+    return post
+
+
+def existing_url(url: str, repo: PostRepository) -> bool:
+    return repo.exists(url)
+
+
 def extract_and_load_post(
     post_id: str,
     url: str,
     source: Source,
     repo: PostRepository,
     filtered: bool = True,
+    overwrite_existing: bool = False,
     validate_target_url: bool = True,
 ) -> None:
     if validate_target_url and not valid_url(url, source):
         raise UrlExtractError("invalid url provided")
 
-    post = extract_post(post_id, url, source)
+    if existing_url(url, repo) and not overwrite_existing:
+        raise UrlExtractError("target url has already been extracted")
 
-    if filtered and filter_post(post, source):
+    extracted_item = source.extractor(url)
+
+    if filtered and filter_item(extracted_item, source):
         raise UrlExtractError(
             "cannot load extracted post: the post failed filter checks"
         )
-    else:
-        try:
-            load_post(post, repo)
-        except ValueError:
-            raise UrlExtractError(
-                "cannot load extracted post: "
-                "the post could not be loaded into the target repository"
-            )
+
+    post = to_post(post_id, extracted_item, source)
+
+    try:
+        load_post(post, repo)
+    except ValueError:
+        raise UrlExtractError(
+            "cannot load extracted post: "
+            "the post could not be loaded into the target repository"
+        )
