7764d4db08ca15476d0f8b0b9ce77b5a55ebf29d
Mark Douthwaite <mark@douthwaite.io>
2024-01-06 23:03:45 +0000

    Add total hits function

--- src/common/keywords.py
+++ src/common/keywords.py
@@ -41,3 +41,9 @@ def count_unique_hits(text: str, keywords: list) -> int:
         if keyword in text:
             count += 1
     return count
+
+
+def count_total_hits(text: str, keywords: list) -> int:
+    tokens = [_.strip() for _ in text.lower().split()]
+    counts = sum(_ in keywords for _ in tokens)
+    return counts
