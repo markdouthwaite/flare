0ffc1e3428c2b2bf85e57af56e77632987b2c4f8
Mark Douthwaite <mark@douthwaite.io>
2024-01-06 21:16:09 +0000

    Add `get_locale` call, use ExtractedItem instead of Post. This prevents expensive calls to Scorers etc. being made unnecessarily.

--- src/common/filters/generic.py
+++ src/common/filters/generic.py
@@ -1,16 +1,20 @@
-from src.entities import Post
+from src.entities import ExtractedItem
 from src.common import keywords
+from src.common.extract.metadata import get_locale
 
 
-def is_english_language(post: Post):
-    if post.locale in ["en", "en-GB", "en-US"]:
+def is_english_language(item: ExtractedItem):
+    locale = get_locale(item.content.text)
+    if locale in ["en", "en-GB", "en-US"]:
         return True
     else:
         return False
 
 
-def mentions_machine_learning(post: Post, min_term_count: int = 2):
-    counts = keywords.count_unique_hits(post.text.lower(), keywords.MACHINE_LEARNING)
+def mentions_machine_learning(item: ExtractedItem, min_term_count: int = 2):
+    counts = keywords.count_unique_hits(
+        item.content.text.lower(), keywords.MACHINE_LEARNING
+    )
     if counts >= min_term_count:
         return True
     else:
