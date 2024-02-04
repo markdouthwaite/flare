47d561cfb906e0dceaa4fa9a43f28d993f2513ee
Mark Douthwaite <mark@douthwaite.io>
2024-02-04 17:34:12 +0000

    Fix import utils

--- flare/cli/develop.py
+++ flare/cli/develop.py
@@ -1,13 +1,14 @@
 import os
 import typer
-import importlib
+import importlib.util
 from flare.commands.posts import load_from_disk
 
 app = typer.Typer()
 
 
 @app.command()
-def load_posts_from_disk(path: str, settings_module: str = "settings"):
-    print(os.listdir("."), os.getcwd())
-    settings = importlib.import_module(settings_module)
+def load_posts_from_disk(path: str, settings_path: str = "settings.py"):
+    spec = importlib.util.spec_from_file_location("settings", settings_path)
+    settings = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(settings)
     load_from_disk(path, settings.POST_REPOSITORY)
