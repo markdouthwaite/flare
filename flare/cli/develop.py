b4e148544cc38379f2ffa5edea9121a0a7500c52
Mark Douthwaite <mark@douthwaite.io>
2024-02-04 17:21:39 +0000

    Debugging utils

--- flare/cli/develop.py
+++ flare/cli/develop.py
@@ -1,3 +1,4 @@
+import os
 import typer
 import importlib
 from flare.commands.posts import load_from_disk
@@ -7,5 +8,6 @@ app = typer.Typer()
 
 @app.command()
 def load_posts_from_disk(path: str, settings_module: str = "settings"):
+    print(os.listdir("."), os.getcwd())
     settings = importlib.import_module(settings_module)
     load_from_disk(path, settings.POST_REPOSITORY)
