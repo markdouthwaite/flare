59f9ac2f38ced1c89cc256d4a19cf8d6c24adba0
Mark Douthwaite <mark@douthwaite.io>
2024-02-17 17:23:05 +0000

    Update import path

--- flare/cli/develop.py
+++ flare/cli/develop.py
@@ -2,7 +2,7 @@ import importlib.util
 
 import typer
 
-from flare.commands.posts import load_from_disk
+from flare.commands import posts
 
 app = typer.Typer()
 
@@ -12,4 +12,4 @@ def load_posts_from_disk(path: str, settings_path: str = "settings.py"):
     spec = importlib.util.spec_from_file_location("settings", settings_path)
     settings = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(settings)
-    load_from_disk(path, settings.POST_REPOSITORY)
+    posts.load_from_disk(path, settings.POST_REPOSITORY)
