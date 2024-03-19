0c500c597b666c4641db1a9123326385f08af318
Mark Douthwaite <mark@douthwaite.io>
2024-03-19 22:09:11 +0000

    Add list endpoint

--- flare/api/links.py
+++ flare/api/links.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, current_app, Response, request, jsonify
 from flare.queue.tasks import extract_and_load_link as _extract_and_load_link
 from flare.core.identifiers import generate_id
+from flare.core.models.queries import QueryFilterSet, QueryFilter, QueryOrderBy
 
 links_blueprint = Blueprint("links", __name__)
 
@@ -19,6 +20,32 @@ def get_rich_link(link_id: str):
 def extract_and_load_link(extractor):
     payload = request.json
     url = payload["url"]
+
     link_id = generate_id()
-    task = _extract_and_load_link.delay(link_id=link_id, url=url, extractor_name=extractor)
+    task = _extract_and_load_link.delay(
+        link_id=link_id, url=url, extractor_name=extractor
+    )
     return jsonify({"id": f"{link_id}", "task": task.id})
+
+
+@links_blueprint.post("/")
+def get_rich_links():
+    rich_links_repo = current_app.config["RICH_LINKS_REPO"]
+    payload = request.json
+
+    if payload.get("filter_set") is not None:
+        filters = [QueryFilter(**_) for _ in payload.get("filter_set")]
+        filter_set = QueryFilterSet(filters=filters)
+    else:
+        filter_set = None
+
+    if payload.get("order_by") is not None:
+        order_by = QueryOrderBy(**payload.get("order_by"))
+    else:
+        order_by = None
+
+    limit = payload.get("limit", 10)
+
+    rich_links = rich_links_repo.list(filter_set=filter_set, order_by=order_by, limit=limit)
+
+    return Response(rich_links.json(), content_type="application/json")
